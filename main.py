eb47289a76a948b1c9d0765a9c4a7d1d77bad35b
vvhatdadogdoin <jifdosggfdoijg@gmail.com>
2025-01-01 18:38:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,63 @@ import functools
 
 from discord.ext import commands
 from colorama import Fore
-from flask import request, jsonify, Flask
+from flask import request, jsonify, Flask, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from queue import Queue
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+owner_id = os.getenv('OWNER_ID')
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 request_queue = [] # the queue
 
 bot = commands.Bot(command_prefix = ">", intents = discord.Intents.all())
 url = "https://hyperskidded-remote-admin.onrender.com/"
 token = os.environ["HSRA_TOKEN"]
+EXCLUDED_PATHS = ["/data-poll"]
+
+AUTHORIZATION_HEADERS = {
+  'Authorization': f'Bearer {token}'
+}
+
+class Whitelist(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  discord_user_id = db.Column(db.String(100), unique=True, nullable=False)
+
+  def __repr__(self):
+    return f'<Whitelist {self.discord_user_id}>'
+  
+class BansWhitelist(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  discord_user_id = db.Column(db.String(100), unique=True, nullable=False)
+
+  def __repr__(self):
+    return f'<BansWhitelist {self.discord_user_id}>'
+  
+class HttpBan(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  player_id = db.Column(db.String(100), unique=True, nullable=False)
+  reason = db.Column(db.String(255), nullable=False)
+  ban_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+  def __repr__(self):
+    return f'<HttpBan {self.player_id} banned for {self.reason}>'
+  
+with app.app_context():
+  db.create_all()
+
+def securitycheck():
+  if request.path in EXCLUDED_PATHS:
+    return None
+
+  api_key = request.headers.get('Authorization')
+  if api_key != f'Bearer {token}':
+    abort(403)
 
 def createSession(channelid, message, sessionname):
   headers = {
@@ -37,7 +84,7 @@ def createSession(channelid, message, sessionname):
 
   response = requests.post(f"https://discord.com/api/v9/channels/{channelid}/threads", headers=headers, json=payload)
 
-def passthroughsessioncheck(): # im making this as a decorator so that it will be easier
+def passthroughsessioncheck(): # ~~im making this as a decorator so that it will be easier~~ no longer used unfortunately
   def decorator(func):
     def checksession(channel: discord.TextChannel) -> bool:
       return isinstance(channel, discord.Thread) and channel.name.startswith("hsra-session-")
@@ -59,7 +106,11 @@ NOTE: This isn't where you should be, as this is only the API for Hyperksidded R
 Contact clientruncontext on Discord for any reports or whitelist requests you have.
   """
 
-@app.route("/data-poll", methods=["GET", "POST"]) # something is wrong with the data poll, so i had to debug it
+@app.beforerequest
+def before_request():
+  securitycheck()
+
+@app.route("/data-poll", methods=["GET", "POST"])
 def pollfordata():
   start_time = time.time()
   print(f"[{Fore.GREEN}Web server{Fore.RESET}]: Data poll has started.")
@@ -70,11 +121,139 @@ def pollfordata():
     if request_queue:
       data = request_queue.pop(0)
       print(f"[{Fore.GREEN}HWeb server{Fore.RESET}]: Recieved data: {data}")
-      return jsonify({"status": "data_received", "params": data})
+      return jsonify({"status": "data_received", "params": data}), 200
       
     time.sleep(0.5)
   
-  return jsonify({"status": "no_data", "params": None})
+  return jsonify({"status": "no_data", "params": None}), 200
+
+@app.route("/whitelist", methods=["POST"])
+def whitelist():
+  data = request.get_json()
+  uid = data.get("user_id")
+
+  if not uid:
+    return jsonify({"status": "error", "message": "User Id is not specified."}), 400
+  
+  try:
+    existing_user = Whitelist.query.filter_by(discord_user_id=uid).first()
+
+    if existing_user:
+      return jsonify({"status": "error", "message": "User is already whitelisted."}), 400
+    
+    new_entry = Whitelist(discord_user_id=uid)
+    db.session.add(new_entry)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "User has been successfully whitelisted."}), 200
+
+  except Exception as err:
+    db.session.rollback()
+    return jsonify({"status": "error", "message": str(err)}), 500
+  
+@app.route("/remove-whitelist", methods=["POST"])
+def removewhitelist():
+  data = request.get_json()
+  uid = data.get("user_id")
+
+  if not uid:
+    return jsonify({"status": "error", "message": "User Id is required."}), 400
+  
+  try:
+    existing_user = Whitelist.query.filter_by(discord_user_id=uid).first()
+
+    if not existing_user:
+      return jsonify({"status": "error", "message": "User has not been whitelisted."}), 400
+    
+    db.session.delete(existing_user)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "Successfully removed user's whitelist."}), 200
+  
+  except Exception as err:
+    db.session.rollback()
+    return jsonify({"status": "error", "message": str(err)}), 500
+  
+@app.route("/get-whitelists", methods=["GET"])
+def getwhitelists():
+  try:
+    whitelist_entries = Whitelist.query.all()
+
+    whitelists = [
+      {
+        "user_id": entry.discord_user_id
+      }
+      for entry in whitelist_entries
+    ]
+
+    return jsonify({"whitelists": whitelists}), 200
+  
+  except Exception as err:
+    return jsonify({"status": "error", "message": str(err)}), 500
+  
+@app.route("/bans-whitelist", methods=["POST"])
+def banswhitelist():
+  data = request.get_json()
+  uid = data.get("user_id")
+
+  if not uid:
+    return jsonify({"status": "error", "message": "User Id is not specified."}), 400
+  
+  try:
+    existing_user = BansWhitelist.query.filter_by(discord_user_id=uid).first()
+
+    if existing_user:
+      return jsonify({"status": "error", "message": "User is already bans whitelisted."}), 400
+    
+    new_entry = BansWhitelist(discord_user_id=uid)
+    db.session.add(new_entry)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "User has been successfully whitelisted to use banning commands."}), 200
+
+  except Exception as err:
+    db.session.rollback()
+    return jsonify({"status": "error", "message": str(err)}), 500
+  
+@app.route("/remove-bans-whitelist", methods=["POST"])
+def removebanswhitelist():
+  data = request.get_json()
+  uid = data.get("user_id")
+
+  if not uid:
+    return jsonify({"status": "error", "message": "User Id is required."}), 400
+  
+  try:
+    existing_user = BansWhitelist.query.filter_by(discord_user_id=uid).first()
+
+    if not existing_user:
+      return jsonify({"status": "error", "message": "User has not been whitelisted to use banning commands."}), 400
+    
+    db.session.delete(existing_user)
+    db.session.commit()
+
+    return jsonify({"status": "success", "message": "Successfully removed user's ban whitelist."}), 200
+  
+  except Exception as err:
+    db.session.rollback()
+    return jsonify({"status": "error", "message": str(err)}), 500
+  
+@app.route("/get-bans-whitelists", methods=["GET"])
+def getbanswhitelists():
+  try:
+    bans_whitelist_entries = BansWhitelist.query.all()
+
+    bans_whitelists = [
+      {
+        "user_id": entry.discord_user_id
+      }
+      for entry in bans_whitelist_entries
+    ]
+
+    return jsonify({"ban_whitelists": bans_whitelists}), 200
+  
+  except Exception as err:
+    return jsonify({"status": "error", "message": str(err)}), 500
 
 @app.route("/send-data", methods=["POST"]) ## the endpoint used to send data to the long poll
 def senddata():
@@ -212,7 +391,7 @@ async def cm(ctx, *, message):
     }
 
     try:
-      requests.post(url + "send-data", json=data, timeout = 40)
+      requests.post(url + "send-data", headers=AUTHORIZATION_HEADERS, json=data, timeout = 40)
       embed = discord.Embed(
         color = discord.Color.green(),
         title = "Success",
@@ -254,7 +433,7 @@ async def csm(ctx, *, message):
     }
 
     try:
-      requests.post(url + "send-data", json=data, timeout = 40)
+      requests.post(url + "send-data", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
       embed = discord.Embed(
         color = discord.Color.green(),
         title = "Success",
@@ -296,7 +475,7 @@ async def ban(ctx, player, *, message):
     }
   
     try:
-      requests.post(url + "send-data", json=data, timeout = 40)
+      requests.post(url + "send-data", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
       embed = discord.Embed(
         color = discord.Color.green(),
         title = "Success",
@@ -339,7 +518,7 @@ async def kick(ctx, player, *, message):
     }
   
     try:
-      requests.post(url + "send-data", json=data, timeout = 40)
+      requests.post(url + "send-data", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
       embed = discord.Embed(
         color = discord.Color.green(),
         description = "Successfully kicked player from session.",
@@ -394,6 +573,158 @@ async def closesession(ctx):
       embed.timestamp = discord.utils.utcnow()
       embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
       await ctx.send(embed=embed)
+
+@bot.command(aliases=["wl"])
+async def whitelist(ctx, user: discord.User):
+  if ctx.author.id == owner_id:
+    data = {
+      "user_id": user.id
+    }
+
+    try:
+      requests.post(url + "whitelist", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
+      embed = discord.Embed(
+        color = discord.Color.green(),
+        description = f"Successfully whitelisted user {user}",
+        title = "Success"
+      )
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+    except Exception as err:
+      embed = discord.Embed(
+        color = discord.Color.red(),
+        title = "Error",
+        description = "An unexpected error has occured."
+      )
+      embed.add_field(name="Details", value=err, inline=False)
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+  else:
+    embed = discord.Embed(
+        color = discord.Color.yellow(),
+        title = "Warning",
+        description = "You do not have the necessary privileges to use this command."
+    )
+    embed.timestamp = discord.utils.utcnow()
+    embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96") 
+    await ctx.send(embed=embed)
+    return
+
+@bot.command(aliases=["bl"])
+async def blacklist(ctx, user: discord.User):
+  if ctx.author.id == owner_id:
+    data = {
+      "user_id": user.id
+    }
+
+    try:
+      requests.post(url + "remove-whitelist", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
+      embed = discord.Embed(
+        color = discord.Color.green(),
+        description = f"Successfully removed {user}'s whitelist.",
+        title = "Success"
+      )
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+    except Exception as err:
+      embed = discord.Embed(
+        color = discord.Color.red(),
+        title = "Error",
+        description = "An unexpected error has occured."
+      )
+      embed.add_field(name="Details", value=err, inline=False)
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+  else:
+    embed = discord.Embed(
+        color = discord.Color.yellow(),
+        title = "Warning",
+        description = "You do not have the necessary privileges to use this command."
+    )
+    embed.timestamp = discord.utils.utcnow()
+    embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96") 
+    await ctx.send(embed=embed)
+    return
+
+@bot.command(aliases=["bbl", "bansbl"])
+async def bansblacklist(ctx, user: discord.User):
+  if ctx.author.id == owner_id:
+    data = {
+      "user_id": user.id
+    }
+
+    try:
+      requests.post(url + "remove-bans-whitelist", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
+      embed = discord.Embed(
+        color = discord.Color.green(),
+        description = f"Successfully removed {user}'s ban whitelist.",
+        title = "Success"
+      )
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+    except Exception as err:
+      embed = discord.Embed(
+        color = discord.Color.red(),
+        title = "Error",
+        description = "An unexpected error has occured."
+      )
+      embed.add_field(name="Details", value=err, inline=False)
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+  else:
+    embed = discord.Embed(
+        color = discord.Color.yellow(),
+        title = "Warning",
+        description = "You do not have the necessary privileges to use this command."
+    )
+    embed.timestamp = discord.utils.utcnow()
+    embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96") 
+    await ctx.send(embed=embed)
+    return
+  
+@bot.command(aliases=["bwl", "banswl", "bwhitelist"])
+async def banswhitelist(ctx, user: discord.User):
+  if ctx.author.id == owner_id:
+    data = {
+      "user_id": user.id
+    }
+
+    try:
+      requests.post(url + "bans-whitelist", json=data, headers=AUTHORIZATION_HEADERS, timeout = 40)
+      embed = discord.Embed(
+        color = discord.Color.green(),
+        description = f"Successfully whitelisted user {user} to use banning commands.",
+        title = "Success"
+      )
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+    except Exception as err:
+      embed = discord.Embed(
+        color = discord.Color.red(),
+        title = "Error",
+        description = "An unexpected error has occured."
+      )
+      embed.add_field(name="Details", value=err, inline=False)
+      embed.timestamp = discord.utils.utcnow()
+      embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96")
+      await ctx.send(embed=embed)
+  else:
+    embed = discord.Embed(
+        color = discord.Color.yellow(),
+        title = "Warning",
+        description = "You do not have the necessary privileges to use this command."
+    )
+    embed.timestamp = discord.utils.utcnow()
+    embed.set_footer(text="Hyperskidded Remote Admin", icon_url="https://cdn.discordapp.com/avatars/1321260594359177267/34279a0c42273e4df6b596a3a5b042f0.webp?size=96") 
+    await ctx.send(embed=embed)
+    return
 
 def main2():  
   if __name__ == "__main__":
